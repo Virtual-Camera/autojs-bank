9ec2df935e3b224bbd68dab98634560949b305d0
selltool <127168437+selltool@users.noreply.github.com>
2026-02-11 08:58:35 +0700

    Update fake_camera.js

--- helpers/fake_camera.js
+++ helpers/fake_camera.js
@@ -22,7 +22,7 @@ const change_video_camera = function (action, data) {
 const select_random_video = function (includeText = [], folder_video = "/sdcard/Movies/FaceData/") {
     try {
         let listVideo = files.listDir(folder_video)
-        LogRelay(name + "select_random_video from folder: " + folder_video + ", list: " + String(listVideo) + ", includeText: " + String(includeText))
+        LogRelay(name + "select_random_video from folder: " + folder_video + ", list: " + JSON.stringify(listVideo) + ", includeText: " + JSON.stringify(includeText))
         let filtedVideo = []
         listVideo.forEach(video => {
             includeStatus = true
